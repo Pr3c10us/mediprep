f1b0a7afb8d4d50ad517f2241fb19e80e4e590a7
Owolabi precious <owo.pre.eno@gmail.com>
2024-10-04 13:07:56 +0100

    complete mediprep

--- src/internals/infrastructure/ports/http/server.ts
+++ src/internals/infrastructure/ports/http/server.ts
@@ -65,9 +65,8 @@ export class Server {
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
             // Handle preflight requests
-            if (req.method == "OPTIONS") {
-                res.status(204)
-                return
+            if (req.method === 'OPTIONS') {
+                return res.sendStatus(200);  // HTTP OK status for the preflight
             }
 
             next();
